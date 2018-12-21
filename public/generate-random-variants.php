43f443d4c0419042026c97572dc855920c1bdcb4
Richard Crooks <email@rocrooks.co.uk>
2018-12-21 15:14:23 +0000

    Find length of sequence before choosing a random position

--- public/generate-random-variants.php
+++ public/generate-random-variants.php
@@ -4,13 +4,15 @@ $testing = true;
 
 //Default to creating 200 variants, using the genes in the installed gene list, and only generating missense variants
 if (isset($tocreate) == false)
-  $tocreate = 200;
+  $tocreate = 20;
 else
   $testing = false;
+
 if (isset($genes) == false)
   $genes = array("ADAMTS10","ADAMTS17","ADAMTSL2","ADAMTSL4","AR","ARX","ATRX","BRAF","BRCA1","BRCA2","CBL","CBS","CBX2","COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","CYP11A1","CYP11B1","CYP17A1","CYP19A1","CYP21A2","DHCR7","DHH","DMRT1","FBN1","FBN2","HRAS","HSD17B2","HSD17B3","HSD3B2","KRAS","LHCGR","MAMLD1","MAP2K1","MAP2K2","MAP3K1","MLH1","MSH2","MSH6","MYH11","NF1","NR0B1","NR5A1","NRAS","POR","PTEN","PTPN11","RAD51C","RAD51D","RAF1","RASA1","RSPO1","SHOC2","SLC2A10","SMAD3","SOS1","SOX9","SPRED1","SRD5A2","SRY","STAR","TGFB2","TGFBR1","TGFBR2","WNT4","WT1");
 else
   $testing = false;
+
 if (isset($types) == false)
   $types = array("Missense");
 else
@@ -42,14 +44,22 @@ while (count($variants) < $tocreate)
 
     $genedetails['Exons'] = $exons;
 
+    //Get the gene sequence from the CDS identified by the ENST ID
+    $genedetails['Sequence'] = getrawdatafromapi($ids['ENST'],"EnsemblSequenceFromENST");
+
     $savedgenedetails[$genesymbol] = $genedetails;
     }
 
+  //Get the exons
+  $exons = $savedgenedetails[$genesymbol]['Exons'];
+  shuffle($exons);
+
+  //Get the size of the nucleotide sequence
+  $last = strlen($savedgenedetails[$genesymbol]['Sequence']);
+  $last = $last['End']-3;
+
   array_push($variants,$genesymbol);
   }
-
-print_r($savedgenedetails);
-echo "<br>";
 
 //Echo variants as array if testing is true
 if ($testing == true)
