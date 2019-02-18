40c821d2878fc82123d3f138afabc3283e950042
Richard Crooks <email@rocrooks.co.uk>
2019-02-18 09:24:48 +0000

    Echo variants once generated

--- public/generate-random-variants.php
+++ public/generate-random-variants.php
@@ -4,12 +4,12 @@ $testing = true;
 
 //Default to creating 200 variants, using the genes in the installed gene list, and only generating missense variants
 if (isset($tocreate) == false)
-  $tocreate = 200;
+  $tocreate = 6;
 else
   $testing = false;
 
 if (isset($genes) == false)
-  $genes = array("ADAMTS10","ADAMTS17","ADAMTSL2","ADAMTSL4","AR","ARX","ATRX","BRAF","BRCA1","BRCA2","CBL","CBS","CBX2","COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","CYP11A1","CYP11B1","CYP17A1","CYP19A1","CYP21A2","DHCR7","DHH","DMRT1","FBN1","FBN2","HRAS","HSD17B2","HSD17B3","HSD3B2","KRAS","LHCGR","MAMLD1","MAP2K1","MAP2K2","MAP3K1","MLH1","MSH2","MSH6","MYH11","NF1","NR0B1","NR5A1","NRAS","POR","PTEN","PTPN11","RAD51C","RAD51D","RAF1","RASA1","RSPO1","SHOC2","SLC2A10","SMAD3","SOS1","SOX9","SPRED1","SRD5A2","SRY","STAR","TGFB2","TGFBR1","TGFBR2","WNT4","WT1");
+  $genes = array("ADAMTS10","ADAMTS17","ADAMTSL2","ADAMTSL4","AR","ARX","ATRX","BRAF","BRCA1","BRCA2","CBL","CBS","CBX2","COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","CYP11A1","CYP11B1","CYP17A1","CYP19A1","CYP21A2","DHCR7","DHH","DMRT1","FBN1","FBN2","HRAS","HSD17B2","HSD17B3","HSD3B2","KRAS","LHCGR","MAMLD1","MAP2K1","MAP2K2","MAP3K1","MLH1","MSH2","MYH11","NF1","NR0B1","NR5A1","NRAS","POR","PTEN","PTPN11","RAD51C","RAD51D","RAF1","RASA1","RSPO1","SHOC2","SLC2A10","SMAD3","SOS1","SOX9","SPRED1","SRY","STAR","TGFB2","TGFBR1","TGFBR2","WNT4");
 else
   $testing = false;
 
@@ -89,7 +89,7 @@ while (count($variants) < $tocreate)
       echo "<br>";
 
     //Add to the list of variants already generated
-    array_push($variants,$genesymbol);
+    array_push($variants,$variantdescription);
     echo $variantdescription;
     }
   }
